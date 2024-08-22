ad892f16882beb1170243ee438209033e931d82f
Matvey <sinema3d00@gmail.com>
2024-08-22 20:41:07 +0300

    add connection to backend

--- Chat/Chat/Program.cs
+++ Chat/Chat/Program.cs
@@ -1,5 +1,24 @@
+using Chat.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
+
+app.UseCors();
+
+app.MapHub<ChatHub>("/chat");
 
 app.Run();
