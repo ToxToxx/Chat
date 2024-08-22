e72e9709534f289d73dc28a81b142acf25381e26
Matvey <sinema3d00@gmail.com>
2024-08-22 20:41:01 +0300

    making frontend part and server connection

--- frontend/chat/src/App.js
+++ frontend/chat/src/App.js
@@ -1,9 +1,29 @@
 import { WaitingRoom } from "./components/WaitingRoom";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 
 function App() {
+  const joinChat = async (userName, chatRoom) => {
+    var connection = new HubConnectionBuilder()
+    .withUrl("http://localhost:5285/chat")
+    .withAutomaticReconnect()
+    .build();
+
+    try{
+      await connection.start();
+      await connection.invoke("JoinChat", {userName, chatRoom});
+      
+      console.log('Prisoedineno');
+      console.log(connection);
+    
+    }catch(error){
+      console.log(error);
+    }
+  };
+
+
   return (
-    <div className="min-h-screen flex items-center bg-gray-100" >
-          <WaitingRoom />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100" >
+          <WaitingRoom joinChat={joinChat}/>
           </div>
   );
 }
--- frontend/chat/src/components/WaitingRoom.jsx
+++ frontend/chat/src/components/WaitingRoom.jsx
@@ -1,10 +1,42 @@
-import {Button, Heading, Input, Text} from "@chakra-ui/react"
+import { Button, Heading, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
-export const WaitingRoom = () => {
-    return <form>
-        <Heading>Онлайн чат</Heading>
-        <div className="">
-            
+export const WaitingRoom = ({joinChat}) => {
+    const [userName, setUserName] = useState();
+    const [chatRoom, setChatRoom] = useState();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        joinChat(userName, chatRoom);
+    }
+    
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <form 
+                onSubmit = {onSubmit}
+                className="max-w-sm w-full bg-white p-8 rounded shadow-lg"
+            >
+                <Heading size="lg" className="mb-6">Онлайн чат</Heading>
+                <div className="mb-4">
+                    <Text fontSize={"sm"}>Имя пользователя</Text>
+                    <Input
+                        onChange={(e) => setUserName(e.target.value)}
+                        name="username"
+                        placeholder="Введите ваше имя"
+                    />
+                </div>
+                <div className="mb-6">
+                    <Text fontSize={"sm"}>Название чата</Text>
+                    <Input
+                        onChange={(e) => setChatRoom(e.target.value)}
+                        name="chatname"
+                        placeholder="Введите название чата"
+                    />
+                </div>
+                <Button type="submit" colorScheme="blue" className="w-full">
+                    Присоединиться
+                </Button>
+            </form>
         </div>
-    </form>
-}
+    );
+};
