42f2014305caa5b57ef381901d0da62903a565d3
Matvey <sinema3d00@gmail.com>
2024-08-24 15:32:43 +0300

    adding redis

--- Chat/Chat/Hubs/ChatHub.cs
+++ Chat/Chat/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using Chat.Models;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
 
 namespace Chat.Hubs
 {
@@ -12,14 +13,20 @@ namespace Chat.Hubs
     public class ChatHub : Hub<IChatClient>
     {
         private readonly IDistributedCache _cache;
+        
         public ChatHub(IDistributedCache cache)
         {
             _cache = cache;
         }
+
         public async Task JoinChat(UserConnection connection)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+
+            var stringConnection = JsonSerializer.Serialize(connection);
             
+            await _cache.SetStringAsync(Context.ConnectionId,stringConnection);
+
             await Clients
                 .Group(connection.ChatRoom)
                 .ReceiveMessage("Admin",$"{connection.UserName} join chat");
@@ -27,8 +34,17 @@ namespace Chat.Hubs
 
         public async Task SendMessage(string message)
         {
-            await Clients
-                .Group()
+            var stringConnection = await _cache.GetAsync(Context.ConnectionId);
+
+            var connection = JsonSerializer.Deserialize<UserConnection>(stringConnection);
+
+            if (connection is not null) 
+            {
+                await Clients
+                .Group(connection.ChatRoom)
+                .ReceiveMessage(connection.UserName, message);
+            }
+
         }
     }
 }
