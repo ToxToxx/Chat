47d714f4fc1036a3ec3a5248078245f5a7ea8022
Matvey <sinema3d00@gmail.com>
2024-08-24 16:48:26 +0300

    fix bugs with docker, made sending messages and close tab

--- Chat/Chat/Hubs/ChatHub.cs
+++ Chat/Chat/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
 ﻿using Chat.Models;
+using Docker.DotNet.Models;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Caching.Distributed;
 using System.Text.Json;
@@ -46,5 +47,25 @@ namespace Chat.Hubs
             }
 
         }
-    }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var stringConnection = await _cache.GetAsync(Context.ConnectionId);
+
+            var connection = JsonSerializer.Deserialize<UserConnection>(stringConnection);
+
+            if (connection is not null)
+            {
+                await _cache.RemoveAsync(Context.ConnectionId);
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, connection.ChatRoom);
+
+                await Clients
+                .Group(connection.ChatRoom)
+                .ReceiveMessage("Admin", $"{connection.UserName} left chat");
+
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    } 
 }
--- frontend/chat/src/components/Chat.jsx
+++ frontend/chat/src/components/Chat.jsx
@@ -1,16 +1,39 @@
-import { CloseButton, Heading} from "@chakra-ui/react";
+import { Button, CloseButton, Heading, Input} from "@chakra-ui/react";
 import { Message } from "./Message";
+import { useState } from "react";
 
-export const Chat = ({messages, chatRoom, closeChat}) => {
-    return <div className="w-1/2 bg-white p-8 rounded shadow-lg">
+
+
+export const Chat = ({ messages, chatRoom, closeChat, sendMessage }) => {
+    const [message, setMessage] = useState("");
+    
+    const onSendMessage = () => {
+        if (message.trim() !== "") { // Проверка на пустое сообщение
+            sendMessage(message);
+            setMessage(""); // Сброс состояния message после отправки
+        }
+    };
+
+    return (<div className="w-1/2 bg-white p-8 rounded shadow-lg">
         <div className="flex flex-row justify-between mb-5">
-            <Heading size = "lg">{chatRoom}</Heading>
-            <CloseButton onClick={closeChat}/>
+            <Heading size="lg">{chatRoom}</Heading>
+            <CloseButton onClick={closeChat} />
         </div>
         <div className="flex flex-col overflow-auto scroll-smooth h-96 gap-3 pb-3">
             {messages.map((messageInfo, index) => (
-                <Message messageInfo={messageInfo} key={index}/>
+                <Message messageInfo={messageInfo} key={index} />
             ))}
         </div>
+        <div className="flex gap-3">
+            <Input 
+            type="text" 
+            value={message} 
+            onChange = {(e)=> setMessage(e.target.value)} 
+            placeholder="Write message"
+            />
+            <Button colorScheme="blue" onClick={onSendMessage}> 
+                Send</Button>
+        </div>
     </div>
+    )
 }
